571bf10df5b0f1a520810c58dc017c1549ac1663
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-09-17 01:53:54 +0530

    Add files via upload

---
+++ IncrementOperators.java
@@ -0,0 +1,17 @@
+public class IncrementOperators {
+    public static void main(String[] args) {
+        int p=10;
+        int q=p++;
+        System.out.println(p);
+        System.out.println(q);
+        System.out.println(p);
+
+        System.out.println(" ");
+        int r=10;
+        int s=++r;
+
+        System.out.println(r);
+        System.out.println(s);
+        System.out.println(r);
+    }
+}
