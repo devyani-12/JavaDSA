67e141f48672422ba3b1ae5499931bc1fd3caedc
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-09-17 01:55:05 +0530

    Add files via upload

---
+++ AddTwoNo.java
@@ -0,0 +1,12 @@
+import java.util.Scanner;
+
+public class AddTwoNo {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        int num1=sc.nextInt();
+        int num2=sc.nextInt();
+        int num3=num1+num2;
+
+        System.out.println("the sum of two numbers is : "+ num3);
+    }
+}
