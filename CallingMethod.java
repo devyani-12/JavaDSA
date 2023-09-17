176b6db3b356f6b2c8c2adeb0875c54ce136105f
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-09-18 02:49:30 +0530

    Add files via upload

---
+++ CallingMethod.java
@@ -0,0 +1,12 @@
+public class CallingMethod {
+    public static void main(String[] args) {
+       welcome();//calling method
+        welcome();
+        welcome();
+        // 3 bar call kia to 3 bar print hoga
+        }
+
+    public static void welcome(){
+        System.out.println("Welcome to JAVA ");
+    }
+}
