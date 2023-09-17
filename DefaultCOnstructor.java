eb8fb99ed71b3407e30aa6e9521c607e771d8e8b
devyani-12 <97430016+devyani-12@users.noreply.github.com>
2023-09-18 02:49:15 +0530

    Add files via upload

---
+++ DefaultCOnstructor.java
@@ -0,0 +1,17 @@
+public class DefaultCOnstructor {
+
+
+    public static  void main(String[] args) {
+        Algebra obj=new Algebra(); //obj bna to default constructor call hua
+
+
+
+    }
+
+}
+class Algebra {
+
+    Algebra(){ //constructor
+        System.out.println("Constructor called");
+    }
+}
